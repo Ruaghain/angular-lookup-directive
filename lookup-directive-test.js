47823ecef8f392fb4ed69558ce7879faff8a9750
ruaghain <ruaghain_dev@hotmail.com>
2015-07-11 15:16:35 +0100

    Changed the code to require ng-model be attached to the directive.

--- lookup-directive-test.js
+++ lookup-directive-test.js
@@ -1,6 +1,6 @@
 describe("Directive: customLookup", function () {
     var $compile,
-        $rootScope,
+        $scope,
         $timeout,
         $httpBackend,
         $q,
@@ -10,12 +10,12 @@ describe("Directive: customLookup", function () {
 
     beforeEach(inject(function (_$rootScope_, _$compile_, _$timeout_, _$httpBackend_, _$q_) {
         $compile = _$compile_;
-        $rootScope = _$rootScope_;
+        $scope = _$rootScope_.$new();
         $timeout = _$timeout_;
         $httpBackend = _$httpBackend_;
         $q = _$q_;
 
-        $rootScope.findUsers = function () {
+        $scope.findUsers = function () {
             return [{
                 "id": 1,
                 "userName": "FirstUser",
@@ -41,48 +41,38 @@ describe("Directive: customLookup", function () {
             }];
         };
 
-        element = angular.element('<custom-lookup lookup-datasource="findUsers" lookup-text-field="fullName" lookup-value-field="id"></custom-lookup>');
-        $compile(element)($rootScope.$new());
+        $scope.user = {userId: null};
+
+        element = angular.element('<custom-lookup ng-model="user.userId" lookup-datasource="findUsers" lookup-text-field="fullName" lookup-value-field="id"></custom-lookup>');
+        $compile(element)($scope);
+        $scope.$digest();
     }));
 
     it("Contains the relevant isolated scope variables, which have been correctly set", function () {
-        $rootScope.$digest();
+
         expect(element.isolateScope().lookupDatasource).toBeDefined();
         expect(element.isolateScope().lookupTextField).toBe("fullName");
         expect(element.isolateScope().lookupValueField).toBe("id");
     });
 
     it("Replaces the element with the appropriate text for lookups", function () {
-        $rootScope.$digest();
         expect(element.find("input").attr("type")).toEqual('text');
     });
 
-    it("Contains the relevant model attribute", function () {
-        $rootScope.$digest();
-        expect(element.find("input").attr("ng-model")).toEqual('searchTerm');
-    });
-
     it("Contains the relevant keyup attribute", function () {
-        $rootScope.$digest();
         expect(element.find("input").attr("ng-keyup")).toEqual('search()');
     });
 
-    it("Contains the relevant angular class attribute values", function () {
-        $rootScope.$digest();
-        expect(element.find("input").attr("class")).toEqual('ng-pristine ng-untouched ng-valid');
-    });
-
     it("Performs search on key up", function () {
-        $rootScope.$digest();
         element.find("input").triggerHandler("keyup");
         expect(element.find("li").length).toEqual(2);
     });
 
-    it("Fills value of selected item in input box.", function () {
-        $rootScope.$digest();
+    it("Text value and associated model of selected item are populated accordingly.", function () {
         element.find("input").triggerHandler("keyup");
         element.find("a").triggerHandler("click");
         expect(element.find("input").val()).toBe("First User");
+        expect($scope.user.userId).toBe(1);
     });
 
 });
--- lookup-directive.js
+++ lookup-directive.js
@@ -11,37 +11,41 @@
     var customLookup = function ($q) {
 
         var template = '<div>' +
-            '<input type="text" ng-model="searchTerm" ng-keyup="search()">' +
-            '<ul>' +
-            '<li ng-repeat="record in foundRecords">' +
-            '<a href="#" data-id="{{record[lookupValueField]}}" ng-click="onItemSelect(record)">{{record[lookupTextField]}}</a>' +
-            '</li>' +
-            '</ul>' +
+                '<input type="text" ng-keyup="search()">' +
+                '<ul>' +
+                '<li ng-repeat="record in foundRecords">' +
+                '<a href="#" data-id="{{record[lookupValueField]}}" ng-click="onItemSelect(record)">{{record[lookupTextField]}}</a>' +
+                '</li>' +
+                '</ul>' +
                 '</div>',
 
-            link = function (scope, element) {
-            scope.onItemSelect = function (item) {
-                element.find("input").val(item[scope.lookupTextField]);
-                scope.foundRecords = [];
-            };
+            link = function (scope, element, attributes, ngModelController) {
+                var input = element.find("input");
 
-            scope.search = function () {
-                var defer = $q.defer();
-                defer.resolve(scope.lookupDatasource()(scope.searchTerm));
-                defer.promise.then(function (searchResults) {
-                    scope.foundRecords = searchResults;
-                });
+                scope.onItemSelect = function (item) {
+                    input.val(item[scope.lookupTextField]);
+                    ngModelController.$setViewValue(item[scope.lookupValueField]);
+                    scope.foundRecords = [];
+                };
+
+                scope.search = function () {
+                    var defer = $q.defer();
+                    defer.resolve(scope.lookupDatasource()(input.val()));
+                    defer.promise.then(function (searchResults) {
+                        scope.foundRecords = searchResults;
+                    });
+                };
             };
-        };
 
         return {
             restrict: "E",
-            replace: true,
             scope: {
                 lookupDatasource: "&",
                 lookupTextField: "@",
                 lookupValueField: "@"
             },
+            replace: true,
+            require: "ngModel",
             template: template,
             link: link
         };
